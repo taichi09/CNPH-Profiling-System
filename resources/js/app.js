97e24483eea95f5240897c8e5918c2041479f43f
ProGrammar1 <verandre3@gmail.com>
2026-02-16 10:23:46 +0800

    sidebar try

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,8 @@
 import './bootstrap';
-
+import 'preline';
+document.addEventListener('DOMContentLoaded', () => {
+  window.HSStaticMethods.autoInit();
+});
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
