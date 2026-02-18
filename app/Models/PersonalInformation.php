f79df89d9cd27fae3d3c395cc8c544c04c5825c7
markbeltran012 <joseph.beltran1314@gmail.com>
2026-02-18 15:59:51 +0800

    push ngani

--- app/Models/PersonalInformation.php
+++ app/Models/PersonalInformation.php
@@ -7,4 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class PersonalInformation extends Model
 {
     //
+    protected $table = 'personal_information';
+
+
+    protected $primaryKey = 'employee_id';
+
+    public $timestamps = false;
+    
+    protected $fillable = [
+        'surname', 'first_name', 'middle_name', 'extension', 
+        'date_of_birth', 'place_of_birth',
+        'sex_at_birth', 'civil_status', 'height', 'weight', 'blood_type', 
+        'umid_id_no', 'pagibig_id_no', 'philhealth_id_no', 'philsys_no',
+        'tin_no', 'agency_employee_no', 'citizenship', 'residential_address',
+        'permanent_zip_code', 'permanent_address', 'permanent_zip_code', 
+        'telephone_no', 'email_address', 'mobile_no'
+    ];
 }
