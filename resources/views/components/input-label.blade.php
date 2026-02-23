5fa474c6a5fe797eb643cd8ed938dd819bd171ed
Taichi <ichirakumen@gmail.com>
2026-02-23 10:33:46 +0800

    removed comments

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,9 +1,3 @@
-{{-- @props(['value'])
-
-<label {{ $attributes->merge(['class' => 'block text-xs font-bold uppercase tracking-widest text-white mb-1']) }}>
-    {{ $value ?? $slot }}
-</label> --}}
-
 @props(['value'])
 
 <label {{ $attributes->twMerge(['class' => 'block text-xs font-bold uppercase tracking-widest text-white mb-1']) }}>
--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,7 +1,3 @@
-{{-- @props(['disabled' => false])
-
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'w-full px-4 py-3 rounded-lg bg-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-white border-0']) }}> --}}
-
 @props(['disabled' => false])
 
 <input @disabled($disabled) {{ $attributes->twMerge(['class' => 'w-full px-4 py-3 rounded-lg bg-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-white border-0']) }}>
