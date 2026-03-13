61bdeb6cda60bbada178b78e8352bb80b33e6cbe
ProGrammar1 <verandre3@gmail.com>
2026-03-13 14:57:50 +0800

    recent_employee

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,7 +8,16 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     //
-    public function index(){
-        return view('dashboard.index');
-    }
+ public function index(){
+    $recentEmployees = \App\Models\PersonalInformation::latest('created_at')
+        ->take(6)
+        ->get()
+        ->map(function($info) {
+            $other = \App\Models\OtherInformation::where('employee_id', $info->employee_id)->first();
+            $info->department_name = $other->department_name ?? 'N/A';
+            return $info;
+        });
+
+    return view('dashboard.index', compact('recentEmployees'));
+}
 }
