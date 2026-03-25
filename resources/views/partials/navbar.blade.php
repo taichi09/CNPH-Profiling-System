766c9c95c53390f19cff582a7faaa38a92d65985
Taichi <ichirakumen@gmail.com>
2026-03-25 11:04:30 +0800

    minor changes

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,17 +1,3 @@
-{{-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-</head>
-<body>
-    
-</body>
-</html> --}}
 <main class="sm:ml-72 p-8">
     
 </main>
