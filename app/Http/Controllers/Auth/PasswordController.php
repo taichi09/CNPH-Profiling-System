122a4337fd17ddb1f7ad3fb48db9f0efbb5c73c2
Taichi <ichirakumen@gmail.com>
2026-04-15 14:04:28 +0800

    edit profile minor changes

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -17,7 +17,15 @@ class PasswordController extends Controller
     {
         $validated = $request->validateWithBag('updatePassword', [
             'current_password' => ['required', 'current_password'],
-            'password' => ['required', Password::defaults(), 'confirmed'],
+            'password' => [
+                'required',
+                'confirmed',
+                Password::min(8)
+                    ->mixedCase()
+                    ->numbers()
+                    ->symbols()
+                    ->uncompromised(),
+            ],
         ]);
 
         $request->user()->update([
--- routes/web.php
+++ routes/web.php
@@ -42,7 +42,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::patch('employees/{id}/resign', [EmployeeController::class, 'resign'])->name('employees.resign');
     Route::patch('employees/{id}/reinstate', [EmployeeController::class, 'reinstate'])->name('employees.reinstate');
     Route::post('/employees/import/preview', [EmployeeController::class, 'importPreview'])
