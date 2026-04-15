49609144b594894cad8c823203b6404f9528c9cc
Taichi <ichirakumen@gmail.com>
2026-04-15 09:23:20 +0800

    changed the password reset link message

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -36,9 +36,6 @@ class PasswordResetLinkController extends Controller
             $request->only('email')
         );
 
-        return $status == Password::RESET_LINK_SENT
-                    ? back()->with('status', __($status))
-                    : back()->withInput($request->only('email'))
-                        ->withErrors(['email' => __($status)]);
+        return back()->with('status', __('If your email exists in our system, a reset link has been sent.'));
     }
 }
