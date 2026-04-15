1a3f1c3d900c44842cc23ac3a3bf73e07ac27136
Taichi <ichirakumen@gmail.com>
2026-04-15 08:55:25 +0800

    add throttling to login; removed unused routes from web.php

--- routes/web.php
+++ routes/web.php
@@ -4,21 +4,12 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\DepartmentController;
-use App\Http\Controllers\PdsOcrController;
-
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
     return redirect()->route('login');
 });
-
-Route::get('/form', function () {
-    return view('add_forms.Form8');
-});
-Route::get('/pds', [PdsOcrController::class, 'index'])->name('pds.index');
-    Route::post('/pds/extract', [PdsOcrController::class, 'extract'])->name('pds.extract');
-
 
 // 2. Wrap all protected routes inside the 'auth' middleware group
 Route::middleware(['auth', 'verified'])->group(function () {
