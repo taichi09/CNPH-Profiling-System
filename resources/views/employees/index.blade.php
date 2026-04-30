47da9d5129cb6f27dab08df57b792762b9ce7302
Taichi <ichirakumen@gmail.com>
2026-04-30 11:24:33 +0800

    export button

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -37,10 +37,14 @@
                         <button type="button" id="import-btn"
                             class="py-2 px-4 inline-flex items-center justify-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50">
                             <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
+                                {{-- <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" /> --}}
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v2h16v-2M12 4v9m0 0l-3-3m3 3l3-3" />
                             </svg>
                             Import
                         </button>
+
+                        {{-- Export Button --}}
+                        @include('employees.partials.export-button')
 
                         {{-- Add Employee Button --}}
                         <a href="{{ route('employees.create.step', 1) }}"
---
+++ resources/views/employees/partials/export-button.blade.php
@@ -0,0 +1,7 @@
+<button type="button"
+    class="py-2 px-4 inline-flex items-center justify-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50">
+    <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
+    </svg>
+    Export
+</button>
